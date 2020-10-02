a7537636a55140552cb45297571ebadf1b3c02a7
Putin <sconner2230@gmail.com>
2020-10-02 12:37:48 +0300

    Перебор элементов

--- src/Main.java
+++ src/Main.java
@@ -1,28 +1,32 @@
 import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) {
-        ArrayList<Integer> arrayList = new ArrayList<Integer>();
+        List<Integer> list = new ArrayList<Integer>();
         int i = 0;
 
-        for ( i = 0; i < 100; i++) {
-            arrayList.add(i);
+        for ( i = 0; i < 10; i++) {
+            list.add(i);
         }
 
-        System.out.println(arrayList.toString());
-        System.out.println(arrayList.get(0));
-        System.out.println(arrayList.get(--i));
-        System.out.println(arrayList.size());
+        System.out.println(list.toString());
+        System.out.println(list.get(0));
+        System.out.println(list.get(--i));
+        System.out.println(list.size());
 
-        for (int j = 0; j < arrayList.size(); j++) {
-            System.out.println(arrayList.get(j));
+        for (int j = 0; j < list.size(); j++) {
+            System.out.println(list.get(j));
         }
 
-        arrayList.remove(5);
+        list.remove(5);
 
-        for(Object s:arrayList){
+        for(Object s:list){
             System.out.println(s);
         }
+
+        list = new LinkedList<Integer>();
     }
 }
